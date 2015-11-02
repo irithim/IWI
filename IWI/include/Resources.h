056db8b20e78dfaed8758be43edadd139f08c37b
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-11-02 18:23:12 +0100

    Load and display an image

--- IWI/include/Resources.h
+++ IWI/include/Resources.h
@@ -1,7 +1,4 @@
 #pragma once
 #include "cinder/CinderResources.h"
 
-//#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
-
-
-
+#define RES_IMAGE			CINDER_RESOURCE(../resources/, cinder_logo.png, 128, IMAGE)
--- IWI/src/IWIApp.cpp
+++ IWI/src/IWIApp.cpp
@@ -1,6 +1,8 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/ImageIo.h"
+#include "Resources.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -12,10 +14,14 @@ class IWIApp : public App {
 	void mouseDown( MouseEvent event ) override;
 	void update() override;
 	void draw() override;
+
+	gl::Texture2dRef processedImageTex_;
 };
 
 void IWIApp::setup()
 {
+	Surface processedImage(loadImage(loadResource(RES_IMAGE)));
+	processedImageTex_ = gl::Texture2d::create(processedImage);
 }
 
 void IWIApp::mouseDown( MouseEvent event )
@@ -28,7 +34,7 @@ void IWIApp::update()
 
 void IWIApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::draw(processedImageTex_, getWindowBounds());
 }
 
 CINDER_APP( IWIApp, RendererGl )
