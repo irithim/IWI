6e126cfed233a8b25cea2e7c365af0d81c403536
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-11-02 20:02:39 +0100

    Refactor

--- IWI/src/IWIApp.cpp
+++ IWI/src/IWIApp.cpp
@@ -9,72 +9,66 @@ using namespace ci::app;
 using namespace std;
 
 struct CustomCircle {
-	vec2 position;
-	float radius;
-	Color color;
+    vec2 position;
+    float radius;
+    Color color;
 };
 
 class IWIApp : public App {
-  public:
-	void setup() override;
-	void mouseDown( MouseEvent event ) override;
-	void update() override;
-	void draw() override;
+    public:
+    void setup() override;
+    void mouseDown(MouseEvent event) override;
+    void update() override;
+    void draw() override;
 
-	gl::Texture2dRef processedImageTex_;
-	vector<CustomCircle> circles_;
+    gl::Texture2dRef processedImageTex_;
+    vector<CustomCircle> circles_;
 
-	const float radius_ = 1.0f;
-	const Color goodColor_ = Color(0.0f, 0.0f, 1.0f);
-	const Color badColor_ = Color(1.0f, 0.0f, 0.0f);
+    const float radius_ = 1.0f;
+    const Color goodColor_ = Color(0.0f, 0.0f, 1.0f);
+    const Color badColor_ = Color(1.0f, 0.0f, 0.0f);
 };
 
-void invertArea(Surface *surface, Area area)
-{
-	Surface::Iter iter = surface->getIter(area);
-	while (iter.line()) {
-		while (iter.pixel()) {
-			iter.r() = 255 - iter.r();
-			iter.g() = 255 - iter.g();
-			iter.b() = 255 - iter.b();
-		}
-	}
+void invertArea(Surface *surface, Area area) {
+    Surface::Iter iter = surface->getIter(area);
+    while (iter.line()) {
+        while (iter.pixel()) {
+            iter.r() = 255 - iter.r();
+            iter.g() = 255 - iter.g();
+            iter.b() = 255 - iter.b();
+        }
+    }
 }
 
-void IWIApp::setup()
-{
-	Surface processedImage(loadImage(loadResource(RES_IMAGE)));
-	invertArea(&processedImage, Area(0, 0, processedImage.getWidth(), processedImage.getHeight()));
-	processedImageTex_ = gl::Texture2d::create(processedImage);
+void IWIApp::setup() {
+    Surface processedImage(loadImage(loadResource(RES_IMAGE)));
+    invertArea(&processedImage, Area(0, 0, processedImage.getWidth(), processedImage.getHeight()));
+    processedImageTex_ = gl::Texture2d::create(processedImage);
 }
 
-void IWIApp::mouseDown( MouseEvent event )
-{
-	CustomCircle circle;
-	circle.position = event.getPos();
-	circle.radius = radius_;
-	if (event.isLeftDown()) {
-		circle.color = goodColor_;
-	}
-	else if (event.isRightDown()) {
-		circle.color = badColor_;
-	}
-	circles_.push_back(circle);
+void IWIApp::mouseDown(MouseEvent event) {
+    CustomCircle circle;
+    circle.position = event.getPos();
+    circle.radius = radius_;
+    if (event.isLeftDown()) {
+        circle.color = goodColor_;
+    } else if (event.isRightDown()) {
+        circle.color = badColor_;
+    }
+    circles_.push_back(circle);
 }
 
-void IWIApp::update()
-{
+void IWIApp::update() {
 }
 
-void IWIApp::draw()
-{
-	gl::clear(Color::black());
-	gl::draw(processedImageTex_, getWindowBounds());
-	for (vector<CustomCircle>::iterator it = circles_.begin(); it != circles_.end(); ++it) {
-		gl::color((*it).color);
-		gl::drawSolidCircle((*it).position, (*it).radius);
-	}
-	gl::color(Color::white());
+void IWIApp::draw() {
+    gl::clear(Color::black());
+    gl::draw(processedImageTex_, getWindowBounds());
+    for (vector<CustomCircle>::iterator it = circles_.begin(); it != circles_.end(); ++it) {
+        gl::color((*it).color);
+        gl::drawSolidCircle((*it).position, (*it).radius);
+    }
+    gl::color(Color::white());
 }
 
-CINDER_APP( IWIApp, RendererGl )
+CINDER_APP(IWIApp, RendererGl)
